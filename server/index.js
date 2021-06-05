003ebbde56b713991da58f061ca39b943b699110
Devon Poston <dev.poston@gmail.com>
2021-06-05 13:48:04 -0700

    dockerized prod proxy

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const PORT = 1337;
-const AddToCart = 'http://localhost:3000';
-const Review = 'http://localhost:1128';
-const Images = 'http://localhost:8080';
+const AddToCart = 'http://13.52.238.216:3000';
+const Review = 'http://3.233.240.212:1128';
+const Images = 'http://3.135.185.185:8080';
 
 app.use(express.static('client/dist'));
 app.use(morgan('dev'));
